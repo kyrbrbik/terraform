9fb3152168bc85858f0c96d1ddc1de0f87784209
kyrbrbik <103336573+kyrbrbik@users.noreply.github.com>
2023-03-05 00:46:10 +0100

    added a disk

--- full-infra/main.tf
+++ full-infra/main.tf
@@ -17,6 +17,11 @@ resource "proxmox_vm_qemu" "node" {
     bridge = "vmbr0"
     model  = "virtio"
   }
+  disk {
+    storage = "local-lvm"
+    type = "virtio"
+    size = "10G"
+  }
   os_type = "cloud-init"
   ipconfig0 = "ip=192.168.0.${each.value}/24,gw=192.168.0.1"
 }
